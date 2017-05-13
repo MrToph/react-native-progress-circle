898b98d5a0a8289b5ade44dd1d91ac8a82cc7418
Christoph Michel <christoph.michel.1992@gmail.com>
2017-05-14 00:38:07 +0200

    Initial Release

---
+++ README.md
@@ -0,0 +1,64 @@
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)]()
+[![Version](https://img.shields.io/npm/v/react-native-progress-circle.svg)](https://www.npmjs.com/package/react-native-progress-circle)
+[![npm](https://img.shields.io/npm/dt/react-native-progress-circle.svg)](https://www.npmjs.com/package/react-native-progress-circle)
+[![Twitter Follow](https://img.shields.io/twitter/follow/cmichelio.svg?style=social&label=Follow)](https://twitter.com/cmichelio)
+
+# React Native Progress Circle
+
+![React Native Progress Circles](/README/featured.png?raw=true "React Native Progress Circles")
+
+## Features
+
+* Custom colors
+* Custom size and border radius
+* Light-weight: No other dependencies besides `react-native`
+
+## Installation
+
+`yarn add react-native-progress-circle`
+
+or
+
+`npm install --save react-native-progress-circle`
+
+## Usage
+
+```javascript
+import ProgressCircle from 'react-native-progress-circle'
+
+render() {
+    return (
+        <ProgressCircle
+            percent={30}
+            radius={50}
+            borderWidth={8}
+            color="#3399FF"
+            shadowColor="#999"
+            bgColor="#fff"
+        >
+            <Text style={{ fontSize: 18 }}>{'30%'}</Text>
+        </ProgressCircle>
+    )
+}
+```
+
+## Props
+| Name | Description | Type | Required | Default Value |
+| :--- | :----- | :--- | :---: | :---: |
+| percent | The percentage used for displaying the progress | Number | ✓ |  |
+| radius | The radius in `px` of the component (including border) | Number | ✓ |  |
+| borderWidth | The border width in `px` | Number | ✓ |  |
+| color | The border color | String |  | '#f00' |
+| shadowColor | The background color of the border | String |  | '#999' |
+| bgColor | The inner background color of the component  | String |  | '#e9e9ef' |
+| children | The children to render inside this component | Node |  | `null` |
+| containerStyle | The custom styling which will be applied to the container of the `children` | Style |  | `null` |
+
+## Author
+
+[Christoph Michel](http://cmichel.io)
+
+
+## License
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,166 @@
+import React, { Component, PropTypes } from 'react'
+import { StyleSheet, View } from 'react-native'
+
+const styles = StyleSheet.create({
+  outerCircle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innerCircle: {
+    overflow: 'hidden',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  leftWrap: {
+    position: 'absolute',
+    top: 0,
+  },
+  halfCircle: {
+    position: 'absolute',
+    top: 0,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+  },
+})
+
+function percentToDegrees(percent) {
+  return percent * 3.6
+}
+
+export default class PercentageCircle extends Component {
+  static propTypes = {
+    color: PropTypes.string,
+    shadowColor: PropTypes.string,
+    bgColor: PropTypes.string,
+    radius: PropTypes.number.isRequired,
+    borderWidth: PropTypes.number,
+    percent: PropTypes.number.isRequired, // eslint-disable-line react/no-unused-prop-types
+    children: PropTypes.node,
+    containerStyle: View.propTypes.style,
+  };
+
+  static defaultProps = {
+    color: '#f00',
+    shadowColor: '#999',
+    bgColor: '#e9e9ef',
+    borderWidth: 2,
+    children: null,
+    containerStyle: null,
+  };
+
+  constructor(props) {
+    super(props)
+    this.state = this.getInitialStateFromProps(props)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(this.getInitialStateFromProps(nextProps))
+  }
+
+  getInitialStateFromProps(props) {
+    const percent = Math.max(Math.min(100, props.percent), 0)
+    const needHalfCircle2 = percent > 50
+    let halfCircle1Degree
+    let halfCircle2Degree
+    // degrees indicate the 'end' of the half circle, i.e. they span (degree - 180, degree)
+    if (needHalfCircle2) {
+      halfCircle1Degree = 180
+      halfCircle2Degree = percentToDegrees(percent)
+    } else {
+      halfCircle1Degree = percentToDegrees(percent)
+      halfCircle2Degree = 0
+    }
+
+    return {
+      halfCircle1Degree,
+      halfCircle2Degree,
+      halfCircle2Styles: {
+        // when the second half circle is not needed, we need it to cover
+        // the negative degrees of the first circle
+        backgroundColor: needHalfCircle2
+          ? this.props.color
+          : this.props.shadowColor,
+      },
+    }
+  }
+
+  renderHalfCircle(rotateDegrees, halfCircleStyles) {
+    const { radius, color } = this.props
+    return (
+      <View
+        style={[
+          styles.leftWrap,
+          {
+            left: radius,
+            width: radius,
+            height: radius * 2,
+          },
+        ]}
+      >
+        <View
+          style={[
+            styles.halfCircle,
+            {
+              left: -radius,
+              width: radius,
+              height: radius * 2,
+              borderRadius: radius,
+              transform: [
+                { translateX: radius / 2 },
+                { rotate: `${rotateDegrees}deg` },
+                { translateX: -radius / 2 },
+              ],
+              backgroundColor: color,
+              ...halfCircleStyles,
+            },
+          ]}
+        />
+      </View>
+    )
+  }
+
+  renderInnerCircle() {
+    const radiusMinusBorder = this.props.radius - this.props.borderWidth
+    return (
+      <View
+        style={[
+          styles.innerCircle,
+          {
+            width: radiusMinusBorder * 2,
+            height: radiusMinusBorder * 2,
+            borderRadius: radiusMinusBorder,
+            backgroundColor: this.props.bgColor,
+            ...this.props.containerStyle,
+          },
+        ]}
+      >
+        {this.props.children}
+      </View>
+    )
+  }
+
+  render() {
+    const {
+      halfCircle1Degree,
+      halfCircle2Degree,
+      halfCircle2Styles,
+    } = this.state
+    return (
+      <View
+        style={[
+          styles.outerCircle,
+          {
+            width: this.props.radius * 2,
+            height: this.props.radius * 2,
+            borderRadius: this.props.radius,
+            backgroundColor: this.props.shadowColor,
+          },
+        ]}
+      >
+        {this.renderHalfCircle(halfCircle1Degree)}
+        {this.renderHalfCircle(halfCircle2Degree, halfCircle2Styles)}
+        {this.renderInnerCircle()}
+      </View>
+    )
+  }
+}
